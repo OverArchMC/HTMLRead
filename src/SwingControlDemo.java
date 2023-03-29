4e571cf2811b960771b300aa22b726e943f1a470
OverArchMC <mussallijoey@gmail.com>
2023-03-29 17:23:48 -0400

    Initial Commit

--- src/SwingControlDemo.java
+++ src/SwingControlDemo.java
@@ -14,8 +14,11 @@ public class SwingControlDemo implements ActionListener {
     private JMenu file, edit, help;
     private JMenuItem cut, copy, paste, selectAll;
     private JTextArea ta, ta2;
+    private JTextField taResult;
     private int WIDTH=800;
     private int HEIGHT=700;
+    private JScrollPane scrollPane;
+    private JTextArea link, term, result;
 
 
     public SwingControlDemo() {
@@ -30,7 +33,7 @@ public class SwingControlDemo implements ActionListener {
     private void prepareGUI() {
         mainFrame = new JFrame("Java SWING Examples");
         mainFrame.setSize(WIDTH, HEIGHT);
-        mainFrame.setLayout(new GridLayout(3, 1));
+        mainFrame.setLayout(new GridLayout(2, 1));
 
 
         cut = new JMenuItem("cut");
@@ -50,23 +53,28 @@ public class SwingControlDemo implements ActionListener {
         edit.add(copy);
         edit.add(paste);
         edit.add(selectAll);
-        mb.add(file);
-        mb.add(edit);
-        mb.add(help);
+        //mb.add(file);
+        //mb.add(edit);
+        //mb.add(help);
 
         ta = new JTextArea();
         ta.setBounds(10, 5, (WIDTH-100)/2, HEIGHT-50);
-        mainFrame.add(mb);
-        mainFrame.add(ta);
+        //mainFrame.add(mb);
+        //mainFrame.add(ta);
         mainFrame.setJMenuBar(mb);
+
+        taResult = new JTextField();
+        //taResult.setBounds(10, HEIGHT-50, WIDTH-10, HEIGHT);
+        //mainFrame.add(taResult);
 
         ta2 = new JTextArea();
         ta2.setBounds(20 + (WIDTH-100)/2, 5, (WIDTH-100)/2, HEIGHT-50);
-        mainFrame.add(ta2);
+        //mainFrame.add(ta2);
 
         headerLabel = new JLabel("", JLabel.CENTER);
         statusLabel = new JLabel("", JLabel.CENTER);
         statusLabel.setSize(350, 100);
+        scrollPane = new JScrollPane();
 
         mainFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent) {
@@ -76,30 +84,75 @@ public class SwingControlDemo implements ActionListener {
         controlPanel = new JPanel();
         controlPanel.setLayout(new FlowLayout());
 
+
+
+
+
+
+
+
+        JPanel border = new JPanel(new BorderLayout());
+        JButton retrieveButton = new JButton("Retrieve");
+        retrieveButton.setActionCommand("Retrieve");
+        retrieveButton.addActionListener(new ButtonClickListener());
+        border.add(retrieveButton, BorderLayout.SOUTH);
+        border.add(new JLabel("Link:", SwingConstants.CENTER), BorderLayout.NORTH);
+
+
+        JPanel spacer = new JPanel(new GridLayout(2, 1)); // [1]
+        JPanel input = new JPanel(new BorderLayout()); // [2]
+
+
+
+        link = new JTextArea(); // 2-1
+        term = new JTextArea(); // 3-2
+        link.setLineWrap(true); // ~
+        input.add(term, BorderLayout.CENTER);
+        input.add(new JLabel("Keyword:", SwingConstants.CENTER), BorderLayout.NORTH);
+        spacer.add(link);
+        spacer.add(input);
+        spacer.setBounds(0, 0, 800, 100);
+        border.add(spacer, BorderLayout.CENTER);
+
+
+
        // mainFrame.add(headerLabel);
-        mainFrame.add(controlPanel);
-        mainFrame.add(statusLabel);
+        //mainFrame.add(controlPanel);
+        //mainFrame.add(scrollPane);
+        //mainFrame.add(statusLabel);
+
+
+        result = new JTextArea();
+        result.setLineWrap(true); // ~
+        result.setEditable(false);
+        scrollPane = new JScrollPane(result);
+
+
+
+        mainFrame.add(border);
+        mainFrame.add(scrollPane);
         mainFrame.setVisible(true);
     }
 
     private void showEventDemo() {
         headerLabel.setText("Control in action: Button");
 
-        JButton okButton = new JButton("OK");
-        JButton submitButton = new JButton("Submit");
-        JButton cancelButton = new JButton("Cancel");
+        JButton okButton = new JButton("Retrieve");
+        JButton submitButton = new JButton("");
+        JButton cancelButton = new JButton("");
+
 
         okButton.setActionCommand("OK");
-        submitButton.setActionCommand("Submit");
-        cancelButton.setActionCommand("Cancel");
+        //submitButton.setActionCommand("Submit");
+        //cancelButton.setActionCommand("Cancel");
 
         okButton.addActionListener(new ButtonClickListener());
         submitButton.addActionListener(new ButtonClickListener());
         cancelButton.addActionListener(new ButtonClickListener());
 
-        controlPanel.add(okButton);
-        controlPanel.add(submitButton);
-        controlPanel.add(cancelButton);
+        //controlPanel.add(okButton);
+        //controlPanel.add(submitButton);
+        //controlPanel.add(cancelButton);
 
         mainFrame.setVisible(true);
     }
@@ -120,16 +173,15 @@ public class SwingControlDemo implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
 
-            if (command.equals("OK")) {
+            if (command.equals("Retrieve")) {
                 //statusLabel.setText("Ok Button clicked.");
-                String url = ta.getText();
-                String contains = ta2.getText();
-                statusLabel.setText(HtmlRead(url, contains));
+                String url = link.getText();
+                String contains = term.getText();
+                scrollPane.createVerticalScrollBar();
+                result.setText(HtmlRead(url, contains));
+                //scrollPane.add();
+                        //.setText(HtmlRead(url, contains));
                 System.out.println(HtmlRead(url, contains));
-            } else if (command.equals("Submit")) {
-                //statusLabel.setText("Submit Button clicked.");
-            } else {
-                //statusLabel.setText("Cancel Button clicked.");
             }
         }
     }
@@ -141,7 +193,7 @@ public class SwingControlDemo implements ActionListener {
             //System.out.print("hello \n");
 
             //String contains = "athletics";
-
+            contains = contains.toLowerCase();
 
 
             URL url = new URL(theurl);
@@ -153,14 +205,14 @@ public class SwingControlDemo implements ActionListener {
                 String[] temp = line.split("\'");
                 for(int i = 0; i < temp.length; i++){
                     if(temp[i].indexOf("https://") == 0 && temp[i].contains(contains)){
-                        result = result + temp[i] + "\n";
+                        result = result + temp[i] + "\n\n";
                     }
 
                 }
                 String[] temp2 = line.split("\"");
                 for(int i = 0; i < temp2.length; i++){
                     if(temp2[i].indexOf("https://") == 0 && temp2[i].contains(contains)){
-                        result += temp2[i];
+                        result += temp2[i] + "\n\n";
                     }
 
                 }
